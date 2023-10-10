f36f21d5903406ff5ae2197047321b745b8133e1
tody-viper78 <mmick8611@gmail.com>
2023-10-10 08:43:21 -0700

    pl

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,14 @@ void print_alphabet_x10(void)
 	char ch;
 	int x;
 
-	i = 0;
-	while (i < 10)
+	x = 0;
+	while (x < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
 		_putchar('\n');
-		i++;
+		x++;
 	}
 }

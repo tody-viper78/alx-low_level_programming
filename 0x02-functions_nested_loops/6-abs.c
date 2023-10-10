8175fce088ea35cd1a9515f34d90645f898d004a
tody-viper78 <mmick8611@gmail.com>
2023-10-10 12:23:54 -0700

    olo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the number to be commuted
+ * Return: absolute number or zero
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
